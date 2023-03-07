1b09c88fb48a866464f555408cb8db36c17627e5
Tom Burley <thomas.burley@bluefruit.co.uk>
2023-03-07 12:13:39 +0000

    Add twos complement utilities

---
+++ source/Drivers/Utilities.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <bitset>
+#include <cstdint>
+#include <cmath>
+
+namespace drivers
+{
+
+class Utilities
+{
+public:
+    template<size_t bits>
+    static constexpr int32_t TwosComplementToInt(std::bitset<bits>& data)
+    {
+        bool negativeValue = data[bits - 1];
+        if (negativeValue)
+        {
+            data.flip();
+            return -static_cast<int32_t>(data.to_ulong()) - 1;
+        }
+
+        return data.to_ulong();
+    }
+
+    template<size_t bits>
+    static constexpr std::bitset<bits> IntToTwosComplement(int32_t value)
+    {
+        if (value >= 0)
+            return std::bitset<bits>(value);
+
+        std::bitset<bits> data(std::abs(value));
+        data.flip();
+        data = std::bitset<bits>(data.to_ulong() + 1);
+        return data;
+    }
+};
+
+}
