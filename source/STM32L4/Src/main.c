de5a54c81a747aa8068424f49149428d16354f3c
Tom Burley <thomas.burley@bluefruit.co.uk>
2023-03-17 15:26:27 +0000

    hal generated spi code

--- source/STM32L4/Src/main.c
+++ source/STM32L4/Src/main.c
@@ -18,6 +18,7 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include "spi.h"
 #include "tim.h"
 #include "usart.h"
 #include "gpio.h"
@@ -87,6 +88,7 @@ int main(void)
   MX_GPIO_Init();
   MX_USART2_UART_Init();
   MX_TIM6_Init();
+  MX_SPI1_Init();
   /* USER CODE BEGIN 2 */
 
   /* USER CODE END 2 */
