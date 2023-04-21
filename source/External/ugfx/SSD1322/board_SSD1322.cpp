b049bc4602fa030d07a0da96223b0a81f4822139
Tom Burley <thomas.burley@bluefruit.co.uk>
2023-04-21 12:19:02 +0100

    Add SSD1322 drivers/docs

---
+++ source/External/ugfx/SSD1322/board_SSD1322.cpp
@@ -0,0 +1,46 @@
+#include "board_SSD1322.h"
+#include "DisplayGpio.h"
+
+static constexpr bool DataState = true;
+static constexpr bool CommandState = false;
+
+extern "C"
+{
+
+void init_board(GDisplay *g)
+{
+    DisplayGpio_SetChipSelect(false);
+}
+
+void setpin_reset(GDisplay *g, gBool state)
+{
+    DisplayGpio_SetResetPin(!state);
+}
+
+void acquire_bus(GDisplay *g)
+{
+    (void) g;
+}
+
+void release_bus(GDisplay *g)
+{
+    (void) g;
+}
+
+void write_cmd(GDisplay *g, gU8 cmd)
+{
+    DisplayGpio_SetDcPin(CommandState);
+    DisplayGpio_Transmit(&cmd, 1);
+}
+
+void write_data(GDisplay *g, gU8 data)
+{
+    DisplayGpio_SetDcPin(DataState);
+    DisplayGpio_Transmit(&data, 1);
+}
+
+void post_init_board(GDisplay *g)
+{
+}
+
+}
---
+++ source/External/ugfx/SSD1322/board_SSD1322.h
@@ -0,0 +1,43 @@
+/*
+ * This file is subject to the terms of the GFX License. If a copy of
+ * the license was not distributed with this file, you can obtain one at:
+ *
+ *              http://ugfx.io/license.html
+ */
+
+#ifndef _GDISP_LLD_BOARD_H
+#define _GDISP_LLD_BOARD_H
+
+//Optional
+//#define SSD1322_USE_DMA
+
+#ifndef SSD1322_USE_DMA
+#define SSD1322_USE_DMA            GFXOFF
+#endif
+
+#include "gfx.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void init_board(GDisplay *g);
+void setpin_reset(GDisplay *g, gBool state);
+void acquire_bus(GDisplay *g);
+void release_bus(GDisplay *g);
+void write_cmd(GDisplay *g, gU8 cmd);
+void write_data(GDisplay *g, gU8 data);
+void post_init_board(GDisplay *g);
+
+#ifdef __cplusplus
+}
+#endif
+
+#if SSD1322_USE_DMA
+void write_data_DMA(GDisplay *g, gU8* data) {
+    (void) g;
+    (void) data;
+}
+#endif    // Use DMA
+
+#endif /* _GDISP_LLD_BOARD_H */
